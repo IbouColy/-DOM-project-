2ba0b8edc1fc000d53842bbfaaf452435b3551b2
IbouColy <ibcoly2@gmail.com>
2023-03-06 15:43:22 +0100

    dom project

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+let     compteur = 0;
+
+function moins(){
+    compteur  =  compteur  -1;
+    document.getElementById('ibou').innerHTML = compteur;
+}
+function plus(){
+    compteur = compteur + 1;
+    document.getElementById('ibou').innerHTML = compteur;
+
+}
+
+let compteur2 =0;
+
+function min(){
+    compteur2 = compteur2 -1;
+    document.getElementById('coly').innerHTML = compteur2;
+}
+function add(){
+    compteur2 = compteur2 +1;
+    document.getElementById('coly').innerHTML = compteur2;
+}
+
+let compteur3 =0;
+
+function nabi() {
+    compteur3 = compteur3  -1;
+    document.getElementById('mane').innerHTML = compteur3;
+
+}
+
+function sene() {
+    compteur3 = compteur3 +1;
+    document.getElementById('mane').innerHTML = compteur3
+}
